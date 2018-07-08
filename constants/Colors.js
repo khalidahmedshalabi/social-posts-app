0f9273f44df7255cb353d59e7ba70ff17cd859cb
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-07-08 19:08:16 +0200

    Add colors and styles

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,5 +1,7 @@
-export const mainColor = '#42b0f4';
+export const mainColor = '#e5aa16';
+export const bgColor = '#201f33';
 
 export default {
-	mainColor
+	mainColor,
+	bgColor
 };
--- constants/Layout.js
+++ constants/Layout.js
@@ -1,9 +1,13 @@
 import { Dimensions } from 'react-native';
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+export const width = Dimensions.get('window').width;
+export const height = Dimensions.get('window').height;
+export const pagePadding = 20
+export const smallPagePadding = 15
 
 export default {
+  pagePadding,
+  smallPagePadding,
   window: {
     width,
     height,
