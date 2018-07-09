1e2c90c9660ec98a14ef0c1d5d0f775ac56344c3
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-07-09 21:27:39 +0200

    Add empty Posts screen

---
+++ containers/Posts/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Container } from 'native-base';
+import { FontAwesome, Foundation, MaterialCommunityIcons } from '@expo/vector-icons';
+import { mainColor, bgColor } from '../../constants/Colors';
+
+
+
+export default class AccountInfo extends Component {
+	render() {
+		return (
+			null
+		)
+	}
+}
