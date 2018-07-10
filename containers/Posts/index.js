abb7a71bda9b5c06987dc94d6da2e48923350d73
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-07-10 19:28:30 +0200

    Add empty Posts screen

--- containers/Posts/index.js
+++ containers/Posts/index.js
@@ -7,6 +7,51 @@ import { mainColor, bgColor } from '../../constants/Colors';
 
 
 export default class AccountInfo extends Component {
+	constructor(props) {
+		super(props)
+
+		/*this.state = {
+			posts: [
+				{
+					key: '1',
+					title: 'المنشور الاول',
+					media_type: 2,
+					media_url: '',
+					link: '',
+					is_completed: ,
+					likes: 
+				},
+				{
+					key: '2',
+					title: 'المنشور الثاني',
+					media_type: 1,
+					media_url: 'https://images1.novica.net/pictures/26/p286562_2_400.jpg',
+					link: '',
+					is_completed: ,
+					likes: 
+				},
+				{
+					key: '3',
+					title: 'المنشور الثالث',
+					media_type: 0,
+					media_url: '',
+					link: '',
+					is_completed: ,
+					likes: 
+				},
+				{
+					key: '4',
+					title: 'المنشور الرابع',
+					media_type: 1,
+					media_url: 'https://images1.novica.net/pictures/26/p286562_2_400.jpg',
+					link: '',
+					is_completed: ,
+					likes: 
+				}
+			]
+		}*/
+	}
+
 	render() {
 		return (
 			null
