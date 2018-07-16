545dc80d437aa132ba2bc8b11be4921dbce20984
Khalid <khalidahmed333@hotmail.com>
2018-07-16 20:11:04 +0200

    Add light background color constant

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,7 +1,9 @@
 export const mainColor = '#FFD700';
 export const bgColor = '#201f33';
+export const lightBGColor = '#474668';
 
 export default {
 	mainColor,
-	bgColor
+	bgColor,
+	lightBGColor
 };
