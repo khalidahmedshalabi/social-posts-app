3ae354bb7a198d258fafc72341345bb3378f4bc4
Khalid <khalidahmed333@hotmail.com>
2018-07-16 20:31:12 +0200

    Add custom header component with a functional back button

---
+++ components/BackHeader/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Platform, StatusBar } from 'react-native'
+import {
+	Header, Left, Body, Button, Right
+} from 'native-base';
+import FontedText from '../FontedText';
+import { mainColor, lightBGColor } from '../../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+
+export default BackHeader = ({ title, navigation, rightComponent }) => (
+	<Header
+		style={{ backgroundColor: lightBGColor, height: 56 + StatusBar.currentHeight, paddingTop: StatusBar.currentHeight }}>
+		<Left style={{ flex: 0.5 }}>
+			<Button
+				transparent
+				onPress={() => navigation.goBack()}
+				style={{ elevation: 0, paddingHorizontal: 10 }}>
+
+				<Ionicons
+					name={Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward'}
+					size={25}
+					color={mainColor} />
+			</Button>
+		</Left>
+
+		<Body style={{ flex: 1, alignItems: 'center' }}>
+			<FontedText style={{ color: mainColor }}>{title}</FontedText>
+		</Body>
+
+		<Right style={{ flex: 0.5 }}>
+			{rightComponent}
+		</Right>
+	</Header>
+)
