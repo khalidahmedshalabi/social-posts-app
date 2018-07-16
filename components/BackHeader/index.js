9db9490346001c8959e7a6a1d4fc8e01e29cfcf6
Khalid <khalidahmed333@hotmail.com>
2018-07-16 20:46:21 +0200

    Attempt to fix header height on iOS devices

--- components/BackHeader/index.js
+++ components/BackHeader/index.js
@@ -7,28 +7,32 @@ import FontedText from '../FontedText';
 import { mainColor, lightBGColor } from '../../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 
-export default BackHeader = ({ title, navigation, rightComponent }) => (
-	<Header
-		style={{ backgroundColor: lightBGColor, height: 56 + StatusBar.currentHeight, paddingTop: StatusBar.currentHeight }}>
-		<Left style={{ flex: 0.5 }}>
-			<Button
-				transparent
-				onPress={() => navigation.goBack()}
-				style={{ elevation: 0, paddingHorizontal: 10 }}>
+export default BackHeader = ({ title, navigation, rightComponent }) => {
+	const statusBarHeight = Platform.OS === 'ios' ? StatusBar.currentHeight * 2 : StatusBar.currentHeight
 
-				<Ionicons
-					name={Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward'}
-					size={25}
-					color={mainColor} />
-			</Button>
-		</Left>
+	return (
+		<Header
+			style={{ backgroundColor: lightBGColor, height: 56 + statusBarHeight, paddingTop: statusBarHeight }}>
+			<Left style={{ flex: 0.5 }}>
+				<Button
+					transparent
+					onPress={() => navigation.goBack()}
+					style={{ elevation: 0, paddingHorizontal: 10 }}>
 
-		<Body style={{ flex: 1, alignItems: 'center' }}>
-			<FontedText style={{ color: mainColor }}>{title}</FontedText>
-		</Body>
+					<Ionicons
+						name={Platform.OS === 'ios' ? 'ios-arrow-forward' : 'md-arrow-forward'}
+						size={25}
+						color={mainColor} />
+				</Button>
+			</Left>
 
-		<Right style={{ flex: 0.5 }}>
-			{rightComponent}
-		</Right>
-	</Header>
-)
+			<Body style={{ flex: 1, alignItems: 'center' }}>
+				<FontedText style={{ color: mainColor }}>{title}</FontedText>
+			</Body>
+
+			<Right style={{ flex: 0.5 }}>
+				{rightComponent}
+			</Right>
+		</Header>
+	)
+}
