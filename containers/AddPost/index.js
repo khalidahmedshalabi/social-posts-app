9423d700f431ef563370419b83e932b4ae0a3108
ZizoHussien <36141034+ZizoHussien@users.noreply.github.com>
2018-07-16 21:37:20 +0200

    Fix Image size

--- containers/AddPost/index.js
+++ containers/AddPost/index.js
@@ -74,8 +74,8 @@ export default class AddPost extends Component {
 			return (
 				<Image
 					source={{ uri: this.state.image }}
-					resizeMode='center'
 					style={{ width: 250, height: 250 }}
+					resizeMode='contain'
 					/>
 			)
 		}
