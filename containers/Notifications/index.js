346c635e7787ed217ace013060e3e5102f730d83
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-07-17 17:33:45 +0200

    Add Notifications screen

---
+++ containers/Notifications/index.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity,FlatList } from 'react-native';
+import { bgColor } from '../../constants/Colors';
+import FontedText from '../../components/FontedText';
+import { Container } from 'native-base';
+
+
+
+export default class Notifications extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			notifications: [
+						{
+							key: '1',
+							status: 0,
+							points: '50'
+						},
+						{
+							key: '2',
+							status: 1,
+							points: '100'
+						},
+						{
+							key: '3',
+							status: 0,
+							points: '150'
+						},
+						{
+							key: '4',
+							status: 1,
+							points: '200'
+						},
+						{
+							key: '5',
+							status: 0,
+							points: '250'
+						},
+						{
+							key: '6',
+							status: 1,
+							points: '300'
+						},
+						{
+							key: '7',
+							status: 0,
+							points: '350'
+						},
+						{
+							key: '8',
+							status: 1,
+							points: '450'
+						},
+						{
+							key: '9',
+							status: 0,
+							points: '500'
+						},
+						{
+							key: '10',
+							status: 1,
+							points: '550'
+						},
+			]
+		}
+	}
+
+
+
+	renderItem = (item) => {
+		return (
+			<View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', paddingVertical: 15, paddingHorizontal: 15 }}>
+				<FontedText style={{ color: 'white', fontSize: 16 }}>{item.status == 0 ? 'لقد تم خصم' : 'لقد تم إضافة'}</FontedText>				
+
+				<FontedText style={{ color: item.status == 0 ? '#c13c3f' : '#4d9336', fontSize: 16, marginLeft: 5 }}>{item.status == 0 ? '-' : '+'}</FontedText>
+				<FontedText style={{ color: item.status == 0 ? '#c13c3f' : '#4d9336', fontSize: 16, marginRight: 5 }}>{item.points}</FontedText>
+
+				<FontedText style={{ color: 'white', fontSize: 16 }}>{item.status == 0 ? 'نقطة من حسابك' : 'نقطة إلى حسابك'}</FontedText>
+			</View>
+		)
+	}
+
+
+	render() {
+		return (
+			<Container style={{ backgroundColor: bgColor }}>
+				<FlatList
+					contentContainerStyle={{
+						paddingVertical: 15
+					}}
+					ItemSeparatorComponent={ () => <View style={{ height: 1, backgroundColor: '#474668', width: '100%' }}></View> }
+					data={this.state.notifications}
+					renderItem={({ item }) => this.renderItem(item)} />
+			</Container>	
+		)
+	}
+}
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -3,10 +3,12 @@ import { createBottomTabNavigator } from 'react-navigation'
 import { mainColor, lightBGColor } from '../constants/Colors'
 import Entypo from 'react-native-vector-icons/Entypo'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 // Screens
 import Posts from '../containers/Posts'
 import Profile from '../containers/Profile'
+import Notifications from '../containers/Notifications'
 
 // Style
 const tabBarIconSize = 27
@@ -16,6 +18,12 @@ export default createBottomTabNavigator({
 		screen: Posts,
 		navigationOptions: {
 			tabBarIcon: ({ tintColor }) => <Entypo color={tintColor} name='news' size={tabBarIconSize} />
+		}
+	},
+	'التنبيهات': {
+		screen: Notifications,
+		navigationOptions: {
+			tabBarIcon: ({ tintColor }) => <Ionicons color={tintColor} name='md-notifications-outline' size={tabBarIconSize} />
 		}
 	},
 	'حسابي': {
