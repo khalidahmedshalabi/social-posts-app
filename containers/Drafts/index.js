6a42c63183aee6c1c8e98c9a6278c0c6f547fbff
Khalid <khalidahmed333@hotmail.com>
2018-07-17 17:07:31 +0200

    Clean & auto indent Drafts\index.js

--- containers/Drafts/index.js
+++ containers/Drafts/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity,FlatList } from 'react-native';
+import { View, TouchableOpacity, FlatList } from 'react-native';
 import { bgColor } from '../../constants/Colors';
 import FontedText from '../../components/FontedText';
 import BackHeader from '../../components/BackHeader';
@@ -10,64 +10,62 @@ import { Container } from 'native-base';
 export default class Drafts extends Component {
 	constructor(props) {
 		super(props)
-		this.state = { FlatListItems: [
+		this.state = {
+			FlatListItems: [
 
-			{key:'1',title: 'عنوان 1',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'2',title: 'عنوان 2',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'3',title: 'عنوان 3',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'4',title: 'عنوان 4',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'5',title: 'عنوان 5',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'6',title: 'عنوان 6',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'7',title: 'عنوان 7',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'8',title: 'عنوان 8',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'9',title: 'عنوان 9',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'10',title: 'عنوان 10',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'11',title: 'عنوان 11',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-			{key:'12',title: 'عنوان 12',msg:'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. '},
-		] }
+				{ key: '1', title: 'عنوان 1', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '2', title: 'عنوان 2', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '3', title: 'عنوان 3', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '4', title: 'عنوان 4', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '5', title: 'عنوان 5', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '6', title: 'عنوان 6', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '7', title: 'عنوان 7', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '8', title: 'عنوان 8', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '9', title: 'عنوان 9', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '10', title: 'عنوان 10', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '11', title: 'عنوان 11', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+				{ key: '12', title: 'عنوان 12', msg: 'محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. محتوى البوست. ' },
+			]
+		}
 	}
 	FlatListItemSeparator = () => {
 		return (
-		  <View
-			style={{
-			  height: 1,
-			  width: "100%",
-				backgroundColor: '#474668',
-			}}
-		  />
+			<View
+				style={{
+					height: 1,
+					width: "100%",
+					backgroundColor: '#474668',
+				}}
+			/>
 		);
-		}
-		
+	}
+
 	render() {
-		/*
-		var date = new Date().getDate();
-      var month = new Date().getMonth() + 1;
-      var year = new Date().getFullYear();*/
 		return (
 			<Container>
-			<BackHeader
-				navigation={this.props.navigation}
-				title='إضافة منشور' />
+				<BackHeader
+					navigation={this.props.navigation}
+					title='إضافة منشور' />
 
-							 <FlatList
-									data={ this.state.FlatListItems }
-									style={{backgroundColor:bgColor,}}
-									//contentContainerStyle={{padding}}
-									ItemSeparatorComponent = {this.FlatListItemSeparator}
-									renderItem={({item}) =>
-									<TouchableOpacity
-									>
-									<View style= {{flex:1,flexDirection:'row'}}>
-									<View style = {{flex:1,alignItems:'flex-start',paddingHorizontal: 15,paddingVertical: 15,}}>
-									<FontedText style={{color:'white',fontSize:16}} > {item.title} </FontedText>
-									<FontedText style={{color:'white',fontSize: 10,textAlign:'left'}} > {item.msg} </FontedText>
-									</View>
-									</View>
-									</TouchableOpacity>
-							 }
-								 />
-				</Container>
-			
+				<FlatList
+					data={this.state.FlatListItems}
+					style={{ backgroundColor: bgColor, }}
+					//contentContainerStyle={{padding}}
+					ItemSeparatorComponent={this.FlatListItemSeparator}
+					renderItem={({ item }) =>
+						<TouchableOpacity
+						>
+							<View style={{ flex: 1, flexDirection: 'row' }}>
+								<View style={{ flex: 1, alignItems: 'flex-start', paddingHorizontal: 15, paddingVertical: 15, }}>
+									<FontedText style={{ color: 'white', fontSize: 16 }} > {item.title} </FontedText>
+									<FontedText style={{ color: 'white', fontSize: 10, textAlign: 'left' }} > {item.msg} </FontedText>
+								</View>
+							</View>
+						</TouchableOpacity>
+					}
+				/>
+			</Container>
+
 		)
 	}
 }
