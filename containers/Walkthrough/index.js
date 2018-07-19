6ba380682ba07269f35e50aebb7dfd37f3f26fc0
karimahmedshalabi <karimahmedshalabi@gmail.com>
2018-07-19 20:51:00 +0200

    Add videos in Walkthrough (issue 33) (incomplete)

--- containers/Walkthrough/index.js
+++ containers/Walkthrough/index.js
@@ -5,6 +5,7 @@ import {
 	Dimensions
 } from 'react-native';
 import { Container, Button } from 'native-base';
+import { Video } from 'expo'
 import * as Animatable from 'react-native-animatable';
 import { FontAwesome, Entypo, MaterialIcons, Ionicons } from '@expo/vector-icons';
 import { mainColor, bgColor } from '../../constants/Colors'
@@ -21,14 +22,14 @@ export default class Walkthrough extends Component {
 	ShouldAnim3=true
 
 	componentDidMount () {
-		this.viewIcon1.fadeOut(0)
+		this.video1.stopAsync()
 		this.viewText1.fadeOut(0)
 		this.viewIcon3.fadeOut(0)
 		this.viewText3.fadeOut(0)
 	}
 
 	applyTheAnim = () => {
-		this.viewIcon1.bounce(1500)
+		this.video1.playAsync()
 		this.viewText1.fadeInUpBig(1500)
 		//this.viewSkip1.fadeInUpBig(1500)
 	}
@@ -46,7 +47,7 @@ export default class Walkthrough extends Component {
 	onPageChanged = (number) => {
 		if(number == 0) {
 			this.viewText1.fadeOut(0)
-			this.viewIcon1.fadeOut(0)
+			//this.video1.stopAsync()
 			//this.viewSkip1.fadeOut(0)
 			
 		}
@@ -63,7 +64,7 @@ export default class Walkthrough extends Component {
 			else if(this.ShouldAnim1==false) 
 			{		
 				this.viewText1.stopAnimation()
-				this.viewIcon1.stopAnimation()
+				//this.video1.stopAsync()
 				this.viewText2.fadeOut(0)
 				this.viewIcon2.fadeOut(0)
 			}
@@ -78,7 +79,7 @@ export default class Walkthrough extends Component {
 			this.applyTheAnim2()
 
 			this.viewText1.fadeOut(0)
-			this.viewIcon1.fadeOut(0)
+			//this.video1.stopAsync()
 			this.ShouldAnim2=false
 		//	this.viewSkip1.fadeOut(0)
 			}
@@ -113,7 +114,7 @@ export default class Walkthrough extends Component {
 
 	render() {
 		return (
-			<Container style={{backgroundColor: bgColor, paddingBottom: 15}}>
+			<Container style={{backgroundColor: bgColor}}>
 				<Swiper
 					height="100%"
 					horizontal={true}
@@ -124,13 +125,19 @@ export default class Walkthrough extends Component {
 					onIndexChanged={(number) => this.onPageChanged(number)}
 				>
 					<View style={styles.contentContainer}>
-						<Animatable.View 
-							style={styles.contentContainerIcon} 
-							animation="rubberBand" 
-							duration={1500}
-							delay={300}>
-							<MaterialIcons name='attach-money' size={120} color={mainColor} />
-						</Animatable.View>
+						<Video
+							//ref={ref => this.video1 = ref} 
+							source={require('../../assets/images/WalkthroughStatsVideo.mp4')}
+							rate={1.0}
+							volume={0.0}
+							resizeMode="contain"
+							isLooping={false}
+							shouldPlay={true}
+							style={{
+								width: '100%',
+								height: 250
+							}}>
+						</Video>
 
 						<Animatable.View style={styles.contentContainerText} animation="fadeInUpBig" >
 							<FontedText style={{ color: 'white', fontSize: 23, textAlign: 'center' }}>هل تريد ان تحصل على المال بدون جهد</FontedText>
@@ -147,11 +154,19 @@ export default class Walkthrough extends Component {
 					</View> 
 
 					<View style={styles.contentContainer}>
-						<Animatable.View 
-							ref={ref => this.viewIcon1 = ref} 
-							style={styles.contentContainerIcon}>
-							<Entypo name='trophy' size={120} color={mainColor} />
-						</Animatable.View>
+						<Video
+							ref={ref => this.video1 = ref} 
+							source={require('../../assets/images/WalkthroughStatsVideo.mp4')}
+							rate={1.0}
+							isMuted={true}
+							resizeMode="contain"
+							isLooping={false}
+							shouldPlay={true}
+							style={{
+								width: '100%',
+								height: 250
+							}}>
+						</Video>
 					
 						<Animatable.View 
 							ref={ref => this.viewText1 = ref} 
@@ -239,7 +254,7 @@ const styles = StyleSheet.create({
 	},
 	contentContainer: {
 		paddingHorizontal: pagePadding,
-		backgroundColor: bgColor,
+		backgroundColor: '#2D2C3A',
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
