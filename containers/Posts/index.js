e327bbb82ae9f870fbef3c66db31c5d9b32ed591
Khalid <khalidahmed333@hotmail.com>
2018-07-21 03:00:35 +0200

    Reset Posts screen back to commit: 131ca5841a75ac088e0bdeca619401a30f5a9a23

--- containers/Posts/index.js
+++ containers/Posts/index.js
@@ -6,30 +6,30 @@ import { mainColor, bgColor } from '../../constants/Colors';
 import FontedText from '../../components/FontedText';
 import { Container } from 'native-base';
 import PopupDialog from 'react-native-popup-dialog';
-
 const height = Dimensions.get('window').height
 
+/*
++use this to show the modal
++
++this.popupDialog.show();
++
++*/
 
 export default class Posts extends Component {
 	constructor(props) {
 		super(props)
 
 		this.state = {
-			NumofPoints:10,
-			VideoEnded:false,
-			HeartOpacity:1,
-			NormalPost:false,
 			posts: [
 				{
 					key: '1',
 					title: 'عنوان المنشور',
 					content: 'محتوى البوست محتوى البوست محتوى البوست محتوى البوست محتوى البوست',
-					media_type: 2 ,
+					media_type: 2,
 					media_url: 'http://mirrors.standaloneinstaller.com/video-sample/small.mp4',
 					link: 'https://google.com',
 					is_completed: 0,
-					likes: 13,
-					is_liked:0,
+					likes: 13
 				},
 				{
 					key: '2',
@@ -39,8 +39,7 @@ export default class Posts extends Component {
 					media_url: 'https://thuocmocrauvatoc.files.wordpress.com/2016/07/rau-dep-16.jpg',
 					link: 'https://facebook.com',
 					is_completed: 0,
-					likes: 89,
-					is_liked:0,
+					likes: 89
 				},
 				{
 					key: '3',
@@ -50,8 +49,7 @@ export default class Posts extends Component {
 					media_url: '',
 					link: 'https://youtube.com',
 					is_completed: 0,
-					likes: 2038,
-					is_liked:1,
+					likes: 2038
 				},
 				{
 					key: '4',
@@ -61,8 +59,7 @@ export default class Posts extends Component {
 					media_url: 'http://mirrors.standaloneinstaller.com/video-sample/grb_2.mp4',
 					link: 'https://google.com',
 					is_completed: 0,
-					likes: 29,
-					is_liked:0,
+					likes: 29
 				},
 				{
 					key: '5',
@@ -72,53 +69,13 @@ export default class Posts extends Component {
 					media_url: 'https://thuocmocrauvatoc.files.wordpress.com/2016/07/rau-dep-16.jpg',
 					link: 'https://twitter.com',
 					is_completed: 1,
-					likes: 2394,
-					is_liked:0,
+					likes: 2394
 				},
-				
-			],
+
+			]
 		}
 	}
-	_onPlaybackStatusUpdate = (playbackStatus) => {
-		if (playbackStatus.didJustFinish){
-			{this.setState({VideoEnded:true})}
-			{this.setState({HeartOpacity:0.7})}
-			
-		}
-	  };
-	  onPressHeart = (key) => {
 
-		const index = this.state.posts.findIndex((el) => el.key === key);
-		
-		if (( this.state.posts[index].is_liked == 0 && this.state.VideoEnded ) || ( this.state.posts[index].is_liked == 0 && this.state.posts[index].media_type !== 2 ))
-		{
-		let copy_posts = [...this.state.posts];
-		
-		copy_posts = copy_posts.filter(post => ({ ...post, Hcolor: 'blue' }))
-		//'#B6B6B6'
-		let post = { ...copy_posts[index] };
-		
-		post.Hcolor = 'red';
-		
-		copy_posts[index] = post;
-		this.setState({
-			posts: copy_posts
-		});
-
-		if(this.state.posts[index].media_type !== 2)
-		{
-			this.setState({NumofPoints:100})
-			this.popupDialog.show();
-		}
-		else
-		{
-			this.setState({NumofPoints:300})
-			this.popupDialog.show();
-		}
-		
-		}
-	}
-	
 	onPressPlayVideo = (key) => {
 		// Find index by key
 		const index = this.state.posts.findIndex((el) => el.key === key);
@@ -138,31 +95,31 @@ export default class Posts extends Component {
 		// Update our copy of posts array
 		copy_posts[index] = post;
 
+		// Update component's state
 		this.setState({
 			posts: copy_posts
 		});
-		// Update component's state
 	}
 
 	renderCorrectMediaComponent = (key, is_playing, media_type, media_url) => {
-		if(media_type == 0) {
+		if (media_type == 0) {
 			// link type
 			return (
-				<View 
-					style={{ 
-						backgroundColor: '#EEEEEE', 
-						width: '100%', 
-						height: 250, 
-						borderTopLeftRadius: 10, 
-						borderTopRightRadius: 10, 
-						justifyContent: 'center', 
-						alignItems: 'center' 
+				<View
+					style={{
+						backgroundColor: '#EEEEEE',
+						width: '100%',
+						height: 250,
+						borderTopLeftRadius: 10,
+						borderTopRightRadius: 10,
+						justifyContent: 'center',
+						alignItems: 'center'
 					}}>
 					<FontedText style={{ color: '#B6B6B6', fontSize: 40 }}>لا يوجد صورة</FontedText>
 				</View>
 			)
 		}
-		else if(media_type == 1) {
+		else if (media_type == 1) {
 			// image type
 			return (
 				<Image
@@ -180,12 +137,10 @@ export default class Posts extends Component {
 			// video type
 			return (
 				<View style={{
-					backgroundColor: 'white', alignItems: 'center', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
+					backgroundColor: 'white', alignItems: 'center', borderTopLeftRadius: 10, borderTopRightRadius: 10
+				}}>
 					<Video
-						ref={this.handleVideoRef}
 						source={{ uri: media_url }}
-						onPlaybackStatusUpdate=
-						{(playbackStatus) => this._onPlaybackStatusUpdate(playbackStatus)}
 						rate={1.0}
 						volume={1.0}
 						isMuted={false}
@@ -193,8 +148,8 @@ export default class Posts extends Component {
 						usePoster={true}
 						shouldPlay={is_playing}
 						isLooping={true}
-						style={{ 
-							width: '100%', 
+						style={{
+							width: '100%',
 							height: 250,
 							borderTopLeftRadius: 10,
 							borderTopRightRadius: 10
@@ -207,28 +162,25 @@ export default class Posts extends Component {
 							:
 							<TouchableOpacity
 								style={{ position: 'absolute', top: 100 }}
-								onPress={() => {
-									this.onPressPlayVideo(key)
-								}
-								}
-								>
-								<FontAwesome 
+								onPress={() => this.onPressPlayVideo(key)}
+							>
+								<FontAwesome
 									name='play-circle'
 									color='#eeeeee'
 									size={70}
-									/>
+								/>
 							</TouchableOpacity>
 					}
 				</View>
 			)
-		}	
+		}
 	}
 
-	renderItem = (item) => { 
+	renderItem = (item) => {
 		return (
 			<View style={{ opacity: item.is_completed == 0 ? null : 0.5 }}>
 				{this.renderCorrectMediaComponent(item.key, item.is_playing, item.media_type, item.media_url)}
-				
+
 				<View style={{ borderBottomLeftRadius: 10, borderBottomRightRadius: 10, backgroundColor: item.is_completed == 0 ? 'white' : '#b5b5b5', justifyContent: 'center', alignItems: 'center', paddingVertical: 15, paddingHorizontal: 12 }}>
 					<FontedText style={{ color: 'black', fontSize: 18, alignSelf: 'flex-start' }}>{item.title}</FontedText>
 
@@ -239,39 +191,32 @@ export default class Posts extends Component {
 							<Feather
 								name='link' size={18} color={'#b5b5b5'} />
 
-							<TouchableOpacity 
+							<TouchableOpacity
 								onPress={() => { Linking.openURL(item.link) }}
 								style={{ marginLeft: 7 }}>
 								<FontedText style={{ color: '#007AF9', fontSize: 15 }}>{item.link}</FontedText>
 							</TouchableOpacity>
 						</View>
-						
+
 						<View style={{ flex: 0.3, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
 							<FontedText style={{ color: mainColor, marginRight: 7, fontSize: 16 }}>{item.likes}</FontedText>
 
-							<MaterialCommunityIcons name='heart' size={20} color={ mainColor } />
+							<MaterialCommunityIcons name='heart' size={20} color={mainColor} />
 						</View>
 					</View>
 				</View>
 
 				{
-					(item.is_completed == 0 ) ? null :
-				<Ionicons 
-					style={{ position: 'absolute', marginTop: height * 0.02, marginLeft: 15 }}
-					name='md-checkmark-circle' size={55} color={'#ff5e5e'} />
+					(item.is_completed == 0) ? null :
+						<Ionicons
+							style={{ position: 'absolute', marginTop: height * 0.02, marginLeft: 15 }}
+							name='md-checkmark-circle' size={55} color={'#ff5e5e'} />
 				}
 
-				<TouchableOpacity 
-				onPress={() => { 
-				{this.onPressHeart(item.key)}
-				if(this.state.VideoEnded){
-					{this.setState({VideoEnded:false})}
-					{this.setState({HeartOpacity:1})}
-					}
-				}}
-					activeOpacity= {this.state.HeartOpacity}
+				<TouchableOpacity
+					activeOpacity={0.7}
 					style={{ position: 'absolute', marginTop: height * 0.35, alignSelf: 'flex-end', paddingRight: 8 }}>
-					<MaterialCommunityIcons name='heart' size={60} color={item.Hcolor} />
+					<MaterialCommunityIcons name='heart' size={60} color={'#B6B6B6'} />
 				</TouchableOpacity>
 			</View>
 		)
@@ -282,7 +227,7 @@ export default class Posts extends Component {
 			<Container style={{ alignItems: 'center', backgroundColor: bgColor, paddingHorizontal: 15 }}>
 				<FlatList
 					contentContainerStyle={{ paddingVertical: 20 }}
-					ItemSeparatorComponent={ () => <View style={{ height: 20 }}></View> }
+					ItemSeparatorComponent={() => <View style={{ height: 20 }}></View>}
 					data={this.state.posts}
 					renderItem={({ item }) => this.renderItem(item)} />
 
@@ -295,12 +240,12 @@ export default class Posts extends Component {
 					<View>
 						<Entypo name='circle-with-plus' size={70} color={'#4d9336'} />
 					</View>
-					
+
 					<View style={{ flexDirection: 'row', marginTop: 15 }}>
 						<FontedText style={{ color: 'white', fontSize: 16 }}>لقد تم إضافة</FontedText>
 
-						<FontedText style={{ color: '#4d9336', fontSize: 16, marginLeft: 5 }}>{this.state.NumofPoints}</FontedText>
-						<FontedText style={{ color: '#4d9336', fontSize: 16, marginRight: 5 }}>+</FontedText>
+						<FontedText style={{ color: '#4d9336', fontSize: 16, marginLeft: 5 }}>10</FontedText>
+						<FontedText style={{ color: '#4d9336', fontSize: 16, marginRight: 5 }}></FontedText>
 
 						<FontedText style={{ color: 'white', fontSize: 16 }}>نقطة إلى حسابك</FontedText>
 					</View>
